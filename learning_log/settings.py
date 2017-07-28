57d4fdb7d7bd565a717c25b24b8588f2d683ab72
matthewrfree <matthewrfree@gmail.com>
2017-07-28 13:25:00 -0700

    Redid Settings to try and fix error

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -133,19 +133,18 @@ BOOTSTRAP3 = {
 
     }
 
-#Heroku Settings
+# Heroku settings
 if os.getcwd() == '/app':
     import dj_database_url
     DATABASES = {
-        'default' : dj_database_url.config(default='postgres://localhost')
-    }
-    #Honor the 'X-Forwarded-Proto' header for request.is_secure().
+        'default': dj_database_url.config(default='postgres://localhost')
+       }
+       
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure().
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-    #Allow all host headers.
+    # Allow all host headers.
     ALLOWED_HOSTS = ['*']
-
-    #Static asset configuration
+    # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
     STATICFILES_DIRS = (
